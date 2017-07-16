24da69caad96942ce33062e99e0330c844e394d4
KnightCoder <rahulvluhar@gmail.com>
2017-07-16 22:31:33 +0530

    Deleted all files Signed-off-by: KnightCoder

--- README.md
+++
@@ -1,1 +0,0 @@
-# knightcoder.github.io
--- app/src/script/app/ctrl.js
+++
@@ -1,8 +0,0 @@
-;(function(angular) {
-    'use strict';
-    angular.module('myApp')
-        .controller('mainCtrl', ['$scope', 'templatesConst', mainCtrlFn]);
-        function mainCtrlFn($scope,templatesConst){
-            $scope.templates=templatesConst;
-        };
-})(window.angular);
